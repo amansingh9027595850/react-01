36509c7c127bd957bfb3904dd0b734d2fab0e2d2
Aman Singh <amansingh90275958@gmail.com>
2026-08-27 22:25:33 +0530

    V2

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  const pageScrolling =(elem)=>{
+    if(elem>0){
+      console.log('scrolling down')
+    }else{
+      console.log('scrolling up')
+    }
+  }
+  
+  return (
+    <div onWheel={(elem)=>{
+      pageScrolling(elem.deltaY)
+    }}>
+     <div className='page1'></div>
+     <div className='page2'></div>
+     <div className='page3'></div>
+    </div>
+  )
+}
+
+export default App
