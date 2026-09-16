34ef98c0a809846ad07b8f81f650335681346471
Aman Singh <amansingh90275958@gmail.com>
2026-09-16 18:59:28 +0530

    smalllevel of pagination implementation

---
+++ 17-gallery-project/src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [userData, setUserData] = useState([]);
+  const [index, setIndex] = useState(1);
+
+  const getDate = async () => {
+    const response = await axios.get(
+      `https://picsum.photos/v2/list?page=${index}&limit=30`,
+    );
+    setUserData(response.data);
+    console.log(userData);
+  };
+  useEffect(() => {
+    getDate();
+  }, [index]);
+
+  let printUserData = (
+    <h3 className="text-gray-400  text-sm">No User Available</h3>
+  );
+
+  if (userData.length > 0) {
+    printUserData = userData.map((elem, idx) => {
+      return (
+        <div key={idx}>
+          <a href={elem.url}>
+            <div className="h-40 w-44  rounded-xl overflow-hidden">
+              <img
+                className="h-full w-full object-cover"
+                src={elem.download_url}
+                alt=""
+              />
+            </div>
+            <div className="text-center font-bold text-lg">{elem.author}</div>
+          </a>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <div className="bg-black text-white overflow-auto p-4 h-screen">
+      <h1 className="text-5xl fixed">{index}</h1>
+      <div className="flex flex-wrap gap-4 items-center p-2">
+        {printUserData}
+      </div>
+      <div className="flex justify-center gap-6 items-center ">
+        <button onClick={()=>{
+          if(index>1){
+            setIndex(index - 1)
+          }
+        }} className="bg-amber-400 w-40 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold">
+          Prev
+        </button>
+        <button onClick={()=>{
+          if(index<34){
+            setIndex(index + 1)
+          }
+        }} className="bg-amber-400 w-40 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold">
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
